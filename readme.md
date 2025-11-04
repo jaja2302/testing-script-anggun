d4def34ea3523a40aaf353ff0b30bc0dbb850abd
jojok <valentinojaja@gmail.com>
2025-11-04 08:50:49 +0700

    startup raspi

---
+++ readme.md
@@ -0,0 +1,6 @@
+cara jalankan
+
+cmd raspi:
+pm2 start ecosystem.config.js
+pm2 startup
+pm2 save
---
+++ startup.js
@@ -0,0 +1,34 @@
+// startup.js
+const { exec } = require('child_process');
+
+// URL yang mau dibuka
+const url = 'https://www.google.com';
+
+// Deteksi sistem operasi dan buka Chrome sesuai platform
+let command;
+
+switch (process.platform) {
+  case 'win32':
+    // Windows
+    command = `start chrome "${url}"`;
+    break;
+  case 'darwin':
+    // macOS
+    command = `open -a "Google Chrome" "${url}"`;
+    break;
+  case 'linux':
+    // Linux (tergantung distro, bisa 'google-chrome' atau 'chromium-browser')
+    command = `google-chrome "${url}" || chromium-browser "${url}"`;
+    break;
+  default:
+    console.error('Platform tidak dikenal. Jalankan manual aja, bro.');
+    process.exit(1);
+}
+
+exec(command, (err) => {
+  if (err) {
+    console.error('Gagal buka Chrome:', err);
+  } else {
+    console.log(`Chrome kebuka otomatis ke ${url}`);
+  }
+});
