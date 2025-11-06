96d1ad104b0f4a2ef8be973824218368ef0a11d7
jaja Valentino <47226119+jaja2302@users.noreply.github.com>
2025-11-06 17:10:35 +0700

    Refactor PM2 config for chrome startup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,10 @@
-// ecosystem.config.js
 module.exports = {
-    apps: [
-      {
-        name: "chrome-startup",          // nama proses di PM2
-        script: "C:\Users\jaja.valentino\Desktop\testing script anggun\startup.js",     
-        interpreter: "node",             // pakai Node.js
-        watch: false,                    // false biar gak restart tiap file berubah
-        autorestart: true,               // true biar kalau error dia restart lagi
-        max_restarts: 5,                 // restart maksimal biar gak infinite loop
-        restart_delay: 2000,             // delay 2 detik sebelum restart
-        env: {
-          NODE_ENV: "production",        // environment variable default
-        },
-        log_date_format: "YYYY-MM-DD HH:mm:ss", // biar log lebih rapi
-      },
-    ],
-  };
-  
+  apps: [{
+    name: "buka_chrome",
+    script: "startup.js",
+    env: {
+      DISPLAY: ":0",
+      XAUTHORITY: "/home/raspiclaybath/.Xauthority"
+    }
+  }]
+};
